d351b4fa9e4f1290e1c9525308c6cccbc9b813ea
codesantaisai <143990097+Jathurshanliwe@users.noreply.github.com>
2024-02-21 12:57:31 +0530

    Repository Pattern Implemented

---
+++ Awushi.Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Awushi.Domain.Contracts;
+using Awushi.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awushi.Infrastructure
+{
+    public static class InfrastructureRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices (this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof (GenericRepository<>));
+            services.AddScoped<ICategoryRepostitory, CategoryRepostitory>();
+            return services;
+        }
+    }
+}
---
+++ Awushi.Infrastructure/Repositories/CategoryRepostitory.cs
@@ -0,0 +1,24 @@
+﻿using Awushi.Domain.Contracts;
+using Awushi.Domain.Models;
+using Awushi.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awushi.Infrastructure.Repositories
+{
+    public class CategoryRepostitory : GenericRepository<Category>, ICategoryRepostitory
+    {
+        public CategoryRepostitory(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            
+        }
+        public async Task Update(Category category)
+        {
+            _dbContext.Update(category);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
